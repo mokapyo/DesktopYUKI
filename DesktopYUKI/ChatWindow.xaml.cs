fbdec21bae3aa4e7f5e6af12bd06d70bcd581f55
piyorz <piyorz@gmail.com>
2017-09-03 22:44:25 +0900

    画像を追加

---
+++ DesktopYUKI/ChatWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DesktopYUKI {
+    /// <summary>
+    /// ChatWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class ChatWindow : Window {
+        public ChatWindow() {
+            InitializeComponent();
+        }
+
+        // 台詞の表示
+        public void DisplayMessage(string message) {
+            Dispatcher.Invoke(() => this.ChatField.Text = message);
+        }
+
+        // 台詞の消去
+        public void ClearMessage() {
+            DisplayMessage(string.Empty);
+        }
+    }
+}
--- DesktopYUKI/MainWindow.xaml.cs
+++ DesktopYUKI/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace DesktopYUKI {
     /// <summary>
@@ -20,6 +22,45 @@ namespace DesktopYUKI {
     public partial class MainWindow : Window {
         public MainWindow() {
             InitializeComponent();
+
+            chatwindow = new ChatWindow();
+            chatwindow.Show();
+
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromSeconds(5.0);
+            timer.Tick += timer_Tick;
+            timer.Start();
+        }
+        DispatcherTimer timer;
+        ChatWindow chatwindow;
+
+        int count = 0;
+        void timer_Tick(object sender, EventArgs e) {
+            count++;
+            if (count == 1) {
+                chatwindow.DisplayMessage("ぬるぽ");
+            } else {
+                count = 0;
+                chatwindow.DisplayMessage("ガッ！！");
+            }
+        }
+
+        protected override void OnClosing(CancelEventArgs e) {
+            base.OnClosing(e);
+            if(chatwindow != null) {
+                chatwindow.Close();
+            }
+        }
+
+        // Clickした時の動作
+        private void Quit_Clicked(object sender, RoutedEventArgs e) {
+            Close();
+        }
+
+        // 掴める機能の追加
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e) {
+            base.OnMouseLeftButtonDown(e);
+            DragMove();
         }
     }
 }
