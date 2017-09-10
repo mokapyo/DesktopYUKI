23e88730bcaedd14f36f4589047d0c2307fe6e03
Moco <Taro@MOCO>
2017-09-10 23:25:01 +0900

    画像のバインディングを追加

--- DesktopYUKI/MainWindow.xaml.cs
+++ DesktopYUKI/MainWindow.xaml.cs
@@ -14,13 +14,16 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Windows.Threading;
+using System.Drawing;
 
 namespace DesktopYUKI {
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
     /// </summary>
     public partial class MainWindow : Window {
-        public MainWindow() {
+		BitmapImage m_MainImage = null;
+
+		public MainWindow() {
             InitializeComponent();
 
             chatwindow = new ChatWindow();
@@ -38,10 +41,20 @@ namespace DesktopYUKI {
         void timer_Tick(object sender, EventArgs e) {
             count++;
             if (count == 1) {
-                chatwindow.DisplayMessage("ぬるぽ");
+				m_MainImage = new BitmapImage();
+				m_MainImage.BeginInit();
+				m_MainImage.UriSource = new Uri("/pict/He.png", UriKind.Relative);
+				m_MainImage.EndInit();
+				MainImage.Source = m_MainImage;
+				chatwindow.DisplayMessage("ぬるぽ");
             } else {
                 count = 0;
-                chatwindow.DisplayMessage("ガッ！！");
+				m_MainImage = new BitmapImage();
+				m_MainImage.BeginInit();
+				m_MainImage.UriSource = new Uri("/pict/He2.png", UriKind.Relative);
+				m_MainImage.EndInit();
+				MainImage.Source = m_MainImage;
+				chatwindow.DisplayMessage("ガッ！！");
             }
         }
 
@@ -59,9 +72,25 @@ namespace DesktopYUKI {
 
         // 掴める機能の追加
         protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e) {
-
             base.OnMouseLeftButtonDown(e);
             DragMove();
-        }
-    }
+			timer.Stop();
+			m_MainImage = new BitmapImage();
+			m_MainImage.BeginInit();
+			m_MainImage.UriSource = new Uri("/pict/Drag.png", UriKind.Relative);
+			m_MainImage.EndInit();
+			MainImage.Source = m_MainImage;
+		}
+
+		protected override void OnMouseLeftButtonUp(MouseButtonEventArgs e)
+		{
+			base.OnMouseLeftButtonUp(e);
+			timer.Start();
+			m_MainImage = new BitmapImage();
+			m_MainImage.BeginInit();
+			m_MainImage.UriSource = new Uri("/pict/He.png", UriKind.Relative);
+			m_MainImage.EndInit();
+			MainImage.Source = m_MainImage;
+		}
+	}
 }
